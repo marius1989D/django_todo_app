a34462e597389155a02c2f9e386faad594bea1c7
Ubuntu <ubuntu@ip-172-31-95-177.ec2.internal>
2019-08-26 15:19:29 +0000

    added Procfile

---
+++ todo/.~c9_invoke_ySZQgx.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+class TestViews(TestCase):
+    def test_get_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "todo_list.html")
+        
+    def test_get_add_item_page(self):
+        page = self.client.get("/add")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
+        
+    def test_get_edit_item_page(self):
+        item
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
